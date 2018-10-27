a81926a48d705a507a1a81d220e24a6925b50663
Matteo Hertel <info@matteohertel.com>
2018-10-27 19:10:16 +0100

    Invoking JS function from Rust

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,4 +1,16 @@
+extern {
+    fn appendToBody(x: u32);
+}
+
+#[no_mangle]
+pub extern fn run() {
+    unsafe{
+      appendToBody(42)
+    }
+}
+
 #[no_mangle]
 pub extern fn add_one(x: u32) -> u32 {
     x + 1
 }
+
